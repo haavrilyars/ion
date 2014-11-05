13483e4064d616e0e441f6e4c53e5a93d7d4ae6b
haavrilyars <sihalala@gmail.com>
2014-11-05 21:11:05 +0700

    Initial commit

---
+++ application/logs/log-2014-11-05.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-11-05 05:29:34 --> Unable to connect to the database
+ERROR - 2014-11-05 05:31:10 --> Error : No XHR access : admin
